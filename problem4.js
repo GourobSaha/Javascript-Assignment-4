5199ea5b2a15bde8f28faaccffba0d521dfe7a8b
Gourob Saha <gourobsaha009@gmail.com>
2022-08-10 23:26:19 +0600

    Fifth Commit

---
+++ problem4.js
@@ -0,0 +1,28 @@
+//Problem - 4: Extra person bus fare check
+//Declaring Function
+
+function publicBusFare(totalPerson){
+
+    const reservedBusContains = 50;
+    const microBusContains = 11;
+    const publicBusFare = 250;
+
+    if(typeof totalPerson !== "number"){
+        return "Please enter number only!";
+    }
+    else if (totalPerson < 0){
+        return "Number of person should be positive!";
+    }
+    else{
+        // calculating remaining persons from reserved bus
+        let extraPersonFromBus = totalPerson % reservedBusContains;
+        // calculating remaining persons from microbus
+        let extraPersonFromMicroBus = extraPersonFromBus % microBusContains;
+        // calculating public bus fare for remaining persons
+        let totalBusFare = extraPersonFromMicroBus * publicBusFare;
+        return totalBusFare;
+    }
+    
+}
+
+console.log(publicBusFare(253));
